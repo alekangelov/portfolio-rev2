3d91f102b4570ab38a78af595ceb2902d8d789e4
Alek Angelov <alek.angelov.sk@gmail.com>
2022-07-19 23:01:36 +0200

    feat: text flex

--- components/Canvas/Scenes/AboutScene.tsx
+++ components/Canvas/Scenes/AboutScene.tsx
@@ -15,6 +15,7 @@ import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 import { scroll } from "@stores";
 import { fontPaths } from "styles/fonts";
+import { columns } from "../Helpers/sizing";
 
 const id = (() => {
   let i = 0;
@@ -35,37 +36,60 @@ export const AboutScene = ({}: SceneProps) => {
   const ref = useRef<THREE.Group>(null);
   useFrame(() => {
     const normalized = scroll.top / 100;
-    ref.current?.position.lerp(v.set(-normalized + 6, 0, 0), 0.2);
+    ref.current?.position.lerp(v.set(-normalized + 6, -2, 0), 0.2);
   });
 
   return (
     <Box
-      mt={2}
+      width="100%"
+      height="100%"
       wrap="no-wrap"
       flexDirection="column"
       alignItems="flex-start"
       justifyContent="flex-start"
     >
-      <BoxDebug />
-      <Box>
-        <BoxDebug />
-
-        <Box alignSelf="flex-start" height={5} ref={ref} flexDirection="row">
-          <Box centerAnchor width={4} mr={0.5} height={5} position={[0, 0, 0]}>
-            <Image scale={[4, 5, 0] as any} url="/images/me/5.jpg" />
-          </Box>
-          <Box centerAnchor width={6} mr={0.5} height={5} position={[0, 0, 3]}>
-            <Image scale={[6, 5, 0] as any} url="/images/me/2.jpg" />
-          </Box>
-          <Box centerAnchor width={4} mr={0.5} height={5} position={[0, 0, 3]}>
-            <Image scale={[4, 5, 0] as any} url="/images/abstract/2.jpg" />
-          </Box>
-          <Box centerAnchor width={6} mr={0.5} height={5} position={[0, 0, -3]}>
-            <Image scale={[6, 5, 0] as any} url="/images/me/3.jpg" />
-          </Box>
-          <Box centerAnchor width={4} mr={0.5} height={5} position={[0, 0, 3]}>
-            <Image scale={[4, 5, 0] as any} url="/images/abstract/4.jpg" />
-          </Box>
+      {/* <BoxDebug /> */}
+      <Box
+        centerAnchor
+        width={columns[7]}
+        height={2}
+        alignSelf="flex-end"
+        alignItems="flex-start"
+        justifyContent="flex-start"
+      >
+        {/* <BoxDebug /> */}
+        <Box mb={0.2}>
+          <Text
+            letterSpacing={-0.1}
+            fontSize={0.5}
+            lineHeight={2}
+            font={fontPaths.manofa.bold}
+          >{`BUT WHO MAKES ALL THIS`}</Text>
+        </Box>
+        <Box>
+          <Text
+            letterSpacing={-0.1}
+            fontSize={1.25}
+            lineHeight={1}
+            font={fontPaths.manofa.bold}
+          >{`\nDOPE SHIT`}</Text>
+        </Box>
+      </Box>
+      <Box alignSelf="flex-start" height={5} ref={ref} flexDirection="row">
+        <Box centerAnchor width={4} mr={0.5} height={5} position={[0, 0, 0]}>
+          <Image scale={[4, 5, 0] as any} url="/images/me/5.jpg" />
+        </Box>
+        <Box centerAnchor width={6} mr={0.5} height={5} position={[0, 0, 3]}>
+          <Image scale={[6, 5, 0] as any} url="/images/me/2.jpg" />
+        </Box>
+        <Box centerAnchor width={4} mr={0.5} height={5} position={[0, 0, 3]}>
+          <Image scale={[4, 5, 0] as any} url="/images/abstract/2.jpg" />
+        </Box>
+        <Box centerAnchor width={6} mr={0.5} height={5} position={[0, 0, -3]}>
+          <Image scale={[6, 5, 0] as any} url="/images/me/3.jpg" />
+        </Box>
+        <Box centerAnchor width={4} mr={0.5} height={5} position={[0, 0, 3]}>
+          <Image scale={[4, 5, 0] as any} url="/images/abstract/4.jpg" />
         </Box>
       </Box>
     </Box>
